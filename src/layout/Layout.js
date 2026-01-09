6045d179a31bd0637c55ede7a68f4e30d34096bb
yezinnie14 <yezinnie14@gmail.com>
2026-01-09 10:17:00 +0900

    update

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -14,8 +14,12 @@ const Layout = () => {
     <div id="app">
       <Header />
       {/* 데스크탑일 때  */}
-        <GnbDesktop /> 
+        
       <main className="layout-main">
+        <section className="sec1">
+        <GnbDesktop /> 
+        <Footer/>
+        </section>
         <section className="layout-outlet">
         <Outlet />
         </section>
@@ -24,7 +28,7 @@ const Layout = () => {
         </section>
       </main>
       
-      <Footer/>
+      
       {/* 모바일일 때 아래 탭 */}
         <GnbMobile />
     </div>
