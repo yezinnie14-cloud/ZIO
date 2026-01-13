e01a4d52ff8408b420eb572baae2089087f94cdc
jegandunghee <jaejea1217@gmail.com>
2026-01-13 17:19:30 +0900

    footer 데스크탑 생성

--- src/pages/Tip.js
+++ src/pages/Tip.js
@@ -1,3 +1,4 @@
+import Footer from "../components/common/Footer";
 import TipCard from "../components/TipCard";
 import './Tip.scss';
 
@@ -16,16 +17,11 @@ const Tip = () => {
     },
     {
       id: 3,
-      title: "예약하신 출차 시간까지 차량 출차를 완료해 주세요.",
-      text: "출차 시간 이후에는 추가 요금이 발생할 수 있습니다.",
-    },
-    {
-      id: 4,
       title: "주차 전, 내부 사진으로 진입 동선을 확인하세요.",
       text: "주차장 내부 사진을 통해 진입 방향과 주변 상황을 미리 확인할 수 있습니다.",
     },
     {
-      id: 5,
+      id: 4,
       title: "장시간·반복 이용 시 정기권이 더 유리해요.",
       text: "누적 이용 시간이 많다면 정기권으로 비용을 절약할 수 있습니다.",
     },
