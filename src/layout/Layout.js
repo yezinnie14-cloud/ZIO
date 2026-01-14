e055c9920e0cac2dd67530b89b92517872e8d6d6
jegandunghee <jaejea1217@gmail.com>
2026-01-14 11:47:54 +0900

    푸터 수정 완료 - 모바일 사이즈

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -5,7 +5,6 @@ import GnbDesktop from "../components/common/GnbDesktop";
 import GnbMobile from "../components/common/GnbMobile";
 import DetailAsidePage from "../pages/section/detail/DetailAsidePage";
 
-  
 const Layout = () => {
   const { pathname } = useLocation();
 
@@ -14,22 +13,24 @@ const Layout = () => {
     <div id="app">
       <Header />
       {/* 데스크탑일 때  */}
-        
+
       <main className="layout-main">
         <section className="sec1">
-        <GnbDesktop /> 
-        <Footer/>
+          <GnbDesktop />
+          <div className="layout-footer">
+            <Footer />
+          </div>
         </section>
         <section className="layout-outlet">
-        <Outlet />
+          <Outlet />
         </section>
         <section className="sec3">
           {isDetail ? <DetailAsidePage /> : null}
         </section>
       </main>
-      
+
       {/* 모바일일 때 아래 탭 */}
-        <GnbMobile />
+      <GnbMobile />
     </div>
   );
 };
